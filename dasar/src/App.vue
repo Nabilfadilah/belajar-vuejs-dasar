d33dea778e97af42cac719827dbb926115bea831
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-07-29 13:08:29 +0700

    Setup project dasar

---
+++ dasar/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello Vue ieu bisa teu?????</h1>
+</template>
+
+<script setup>
+console.log("Hello from App.vue");
+</script>
+
+<style scoped>
+h1 {
+  color: dodgerblue;
+}
+</style>
---
+++ dasar/src/components/HelloVue.vue
@@ -0,0 +1,13 @@
+<template>
+  <H1 class="hello">Kuduna bisa ieumah Hello.html</H1>
+</template>
+
+<script setup>
+    console.log("Hello Sarua keneh nong");
+</script>
+
+<style scoped>
+.hello {
+    color: sienna;
+}
+</style>
