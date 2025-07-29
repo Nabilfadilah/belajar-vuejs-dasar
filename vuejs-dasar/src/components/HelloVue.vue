cd7ac76377b10561c86892597f38befd77f42d82
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-07-29 13:11:49 +0700

    Membuat component hello vue

---
+++ vuejs-dasar/src/components/HelloVue.vue
@@ -0,0 +1,13 @@
+<template>
+  <H1 class="hello">Hello Vue dari hello.html</H1>
+</template>
+
+<script setup>
+    console.log("Hello Vue");
+</script>
+
+<style scoped>
+.hello {
+    color: green;
+}
+</style>
