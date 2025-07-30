de16c6250a88e04f89472ab73f7a11e34bce7a40
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-07-30 14:35:06 +0700

    Attribute Binding untuk menampilkan variabel dengan atribute

---
+++ vuejs-dasar/src/components/AttributeBinding.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- 
+    - Mustache tidak bisa digunakan pada attribute di Element
+    - Jika kita ingin menggunakan variable pada attribute di Element, kita perlu menggunakan Directive v-bind:nama-attribute
+    - Dan karena penggunaan v-bind itu sering sekali dilakukan, maka Vue juga menyediakan shortcut untuk menggunakan v-bind, cukup gunakan : (titik dua) diikuti dengan nama attribute nya, misal :id, :class, dan lain-lain
+    - Bahkan jika kita ingin menggunakan variable yang sama dengan nama attribute, kita tidak perlu menggunakan :id=”id”, kita bisa langsung menggunakan :id saja, begitu juga dengan nama attribute yang lain 
+    -->
+  <div>
+    <!-- atribute dan nama variabel -->
+    <h1 :id="id" v-bind:class="classHello">{{ hello }}</h1>
+    <div :class="classHello" v-html="classHello"></div>
+  </div>
+</template>
+
+<script setup>
+// nama variabel dengan
+const hello = "Perkenalkan saya Nabil";
+const classHello = "Hello ini Fadilah";
+
+// atribute dan nama variabel sama
+const id = "Mohammad";
+</script>
+
+<style scoped>
+.hello {
+    color: aqua;
+}
+</style>
--- vuejs-dasar/src/hello.js
+++ vuejs-dasar/src/hello.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import HelloVue from "./components/HelloVue.vue";
 import TextInterpolation from "./components/TextInterpolation.vue";
 import RawHtml from "./components/RawHtml.vue";
+import AttributeBinding from "./components/AttributeBinding.vue";
 
 // untuk menampilkan component
-createApp(HelloVue).mount("#app");
-createApp(TextInterpolation).mount("#app");
-createApp(RawHtml).mount("#app");
+// createApp(HelloVue).mount("#app");
+// createApp(TextInterpolation).mount("#app");
+// createApp(RawHtml).mount("#app");
+createApp(AttributeBinding).mount("#app");
