d050c01f5350275396ceda7467da43dff7d98fe7
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-07-30 14:08:00 +0700

    Text interpolation untuk menampilkan di template

---
+++ vuejs-dasar/src/components/TextInterpolation.vue
@@ -0,0 +1,16 @@
+<script setup>
+    console.log("Hello Vue");
+
+    const hello = "<b>Hello Text Interpolation<b>"
+</script>
+
+<template>
+    <!-- mengambil variabel hello -->
+  <H1 class="hello">{{ hello }}</H1> 
+</template>
+
+<style scoped>
+.hello {
+    color: rebeccapurple;
+}
+</style>
--- vuejs-dasar/src/hello.js
+++ vuejs-dasar/src/hello.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import HelloVue from "./components/HelloVue.vue";
+import TextInterpolation from "./components/TextInterpolation.vue";
 
 // untuk menampilkan component
 createApp(HelloVue).mount("#app");
+createApp(TextInterpolation).mount("#app");
