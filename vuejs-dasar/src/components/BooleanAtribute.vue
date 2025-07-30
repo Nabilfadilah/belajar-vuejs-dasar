6cec49fa88561c7a215da6a84c052cf6af02e283
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-07-30 14:42:37 +0700

    Boolean Attribute untuk menampilkan variabel dengan kondisi

---
+++ vuejs-dasar/src/components/BooleanAtribute.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <!-- Di beberapa kasus, ada attribute yang value nya adalah boolean (true / false)
+Contoh misal nya attribute disabled dan checked pada element Input
+Pada kasus ini, Directive v-bind juga mendukung value boolean -->
+
+  <div>
+    <!-- atribute dan nama variabel -->
+    <h1 :id="id" v-bind:class="classHello">{{ hello }}</h1>
+    <div :class="classHello" v-html="classHello"></div>
+
+    <!-- element button yang disable -->
+     <button class="button" :disabled="buttonDisable">This is Button Disable</button>
+  </div>
+</template>
+
+<script setup>
+// nama variabel dengan
+const hello = "Perkenalkan saya Nabil";
+const classHello = "Hello ini Fadilah";
+
+// atribute dan nama variabel sama
+const id = "Mohammad";
+
+// boolean attribute
+const buttonDisable = true;
+</script>
+
+<style scoped>
+.hello {
+    color: aqua;
+}
+
+.button {
+    margin-top: 10px;
+    padding: 7px;
+    cursor: pointer;
+    color: white;
+}
+</style>
--- vuejs-dasar/src/hello.js
+++ vuejs-dasar/src/hello.js
@@ -3,9 +3,11 @@ import HelloVue from "./components/HelloVue.vue";
 import TextInterpolation from "./components/TextInterpolation.vue";
 import RawHtml from "./components/RawHtml.vue";
 import AttributeBinding from "./components/AttributeBinding.vue";
+import BooleanAtribute from "./components/BooleanAtribute.vue";
 
 // untuk menampilkan component
 // createApp(HelloVue).mount("#app");
 // createApp(TextInterpolation).mount("#app");
 // createApp(RawHtml).mount("#app");
-createApp(AttributeBinding).mount("#app");
+// createApp(AttributeBinding).mount("#app");
+createApp(BooleanAtribute).mount("#app");
