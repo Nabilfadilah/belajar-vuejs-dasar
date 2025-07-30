4c8bac494a4d61baab3b576e3d20fbc8915f7b38
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-07-30 14:12:20 +0700

    Raw HTML untuk menampilkan di template

---
+++ vuejs-dasar/src/components/RawHtml.vue
@@ -0,0 +1,16 @@
+<script setup>
+    console.log("Hello Vue");
+
+    const rawHtml = "<h1>Hello Raw HTML<h1>"
+</script>
+
+<template>
+    <!-- raw html -->
+    <div v-html="rawHtml"></div>
+</template>
+
+<style scoped>
+.hello {
+    color: rebeccapurple;
+}
+</style>
--- vuejs-dasar/src/hello.js
+++ vuejs-dasar/src/hello.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import HelloVue from "./components/HelloVue.vue";
 import TextInterpolation from "./components/TextInterpolation.vue";
+import RawHtml from "./components/RawHtml.vue";
 
 // untuk menampilkan component
 createApp(HelloVue).mount("#app");
 createApp(TextInterpolation).mount("#app");
+createApp(RawHtml).mount("#app");
