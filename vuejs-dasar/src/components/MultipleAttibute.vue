d73fb11999ffac455e54cd5023bd0111c8a50655
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-07-30 14:50:18 +0700

    Multiple Attributes untuk menampilkan variabel dengan data object

---
+++ vuejs-dasar/src/components/MultipleAttibute.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- atribute dan nama variabel -->
+    <h1 v-bind="data">{{ hello }}</h1>
+    <div :class="data.id" v-html="hello"></div>
+
+    <!-- element button yang disable -->
+     <button class="button" :disabled="buttonDisable">This is Button Disable</button>
+  </div>
+</template>
+
+<script setup>
+const hello = "INI Multiple Attribute"
+
+// boolean attribute
+const buttonDisable = true;
+
+// multipe attribute data object
+const data = {
+    id: "asas",
+    class: "hello"
+}
+</script>
+
+<style scoped>
+.hello {
+    color: aqua;
+}
+
+.button {
+    margin-top: 10px;
+    padding: 7px;
+    cursor: pointer;
+    color: white;
+}
+</style>
--- vuejs-dasar/src/hello.js
+++ vuejs-dasar/src/hello.js
@@ -4,10 +4,12 @@ import TextInterpolation from "./components/TextInterpolation.vue";
 import RawHtml from "./components/RawHtml.vue";
 import AttributeBinding from "./components/AttributeBinding.vue";
 import BooleanAtribute from "./components/BooleanAtribute.vue";
+import MultipleAttibute from "./components/MultipleAttibute.vue";
 
 // untuk menampilkan component
 // createApp(HelloVue).mount("#app");
 // createApp(TextInterpolation).mount("#app");
 // createApp(RawHtml).mount("#app");
 // createApp(AttributeBinding).mount("#app");
-createApp(BooleanAtribute).mount("#app");
+// createApp(BooleanAtribute).mount("#app");
+createApp(MultipleAttibute).mount("#app");
