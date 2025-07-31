2bbf52f79300ea84d513bf17992df8df01d6cdf9
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-07-31 13:07:21 +0700

    Tugas 4: memberi ucapan selamat berdasarkan score, mengunakan kondisi

---
+++ vuejs-dasar/src/components/tugas4/Score.vue
@@ -0,0 +1,34 @@
+<!-- membuat halaman web untuk memberi ucapan selamat berdasarkan score, mengunakan kondisi  -->
+
+<script setup>
+    import { ref } from 'vue';
+
+    const score = ref(80);
+</script>
+
+<template>
+    <div>
+        <h1>Score App</h1>
+
+        <!-- v-show, element akan ditambahkan ke DOM -->
+         <div v-show="score > 50">
+            <p>Ini tampilan dengan v-show Score:<b>{{ score }}</b></p>
+         </div>
+
+        <div v-if="score > 90">
+            <p>Yeyy cool score <b>{{ score }}</b></p>
+        </div>
+
+        <div v-else-if="score > 70">
+            <p>Good score <b>{{ score }}</b></p>
+        </div>
+
+        <div v-else>
+            <p>Ups, bad score HAHAHA :D <b>{{ score }}</b></p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vuejs-dasar/src/score.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+
+import Score from "./components/tugas4/Score.vue";
+
+createApp(Score).mount("#app");
