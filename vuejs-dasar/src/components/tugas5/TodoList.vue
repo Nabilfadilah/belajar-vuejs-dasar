c76e087e336bd8b1a3afb286c760265ba0193587
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-07-31 13:53:05 +0700

    Tugas 5: membuat halaman Todo List, dimana data Todo List akan kita simpan dalam array di JavaScript

---
+++ vuejs-dasar/src/components/tugas5/TodoList.vue
@@ -0,0 +1,82 @@
+<!-- membuat halaman Todo List, dimana data Todo List akan kita simpan dalam array di JavaScript -->
+
+<script setup>
+const items = [
+  {
+    id: 1,
+    text: "Learn Vue 3",
+    done: true,
+  },
+  {
+    id: 2,
+    text: "Learn Vue Router",
+    done: false,
+  },
+  {
+    id: 3,
+    text: "Learn Vuex",
+    done: false,
+  },
+];
+</script>
+
+<template>
+  <div>
+    <div>
+      <h1>List Rendering</h1>
+      <ul>
+        <!-- Gunakan :key yang unik untuk setiap elemen yang diulang dengan v-for -->
+        <li v-for="item in items" :key="item.id">
+          {{ item.text }}
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h1>List for Object</h1>
+      <ul v-for="item in items" :key="item.id">
+        <li v-for="(value, key) in item" :key="key">
+          <!-- key = properti dari object -->
+          {{ key }}: {{ value }}
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h1>List for Range</h1>
+      <!-- <ul v-for="item in items" :key="item.id">
+        <li v-for="(value, key) in item" :key="key">
+          {{ key }}: {{ value }}
+        </li>
+      </ul> -->
+
+      <div v-for="n in 10" :key="n">
+        <h4>Hello ini List for range {{ n }}</h4>
+      </div>
+    </div>
+
+    <div>
+      <h1>List v-for dan v-if</h1>
+
+      <!-- ini yang SALAH -->
+      <!-- <ul v-for="item in items" :key="item.id" v-if="!item.done">
+        <li v-for="(value, key) in item" :key="key">{{ key }}: {{ value }}</li>
+      </ul> -->
+
+      <!-- ini yang BENAR -->
+      <template v-for="item in items">
+        <ul :key="item.id" v-if="!item.done">
+          <li v-for="(value, key) in item" :key="key">
+            {{ key }}: {{ value }}
+          </li>
+        </ul>
+      </template>
+
+      <div v-for="n in 10" :key="n">
+        <h4>Hello ini List v-for dan v-if {{ n }}</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vuejs-dasar/src/todo-list.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+
+import TodoList from "./components/tugas5/TodoList.vue";
+
+createApp(TodoList).mount("#app");
