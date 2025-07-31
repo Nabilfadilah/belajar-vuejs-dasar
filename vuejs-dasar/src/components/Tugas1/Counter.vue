e162a94683238e8581e81ba67557af477a25ea59
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-07-31 07:21:50 +0700

    Tugas 1: menampilkan data counter

---
+++ vuejs-dasar/src/components/Tugas1/Counter.vue
@@ -0,0 +1,24 @@
+<!-- halaman yang menampilkan data Counter -->
+<script setup>
+
+let count = 0;
+
+function increment() {
+    count ++;
+    
+    // gunakan manipulasi DOM
+    document.getElementById('count').innerText = `Counter : ${count}`;
+}
+
+</script>
+
+<template>
+    <div>
+        <h1 id="count">Counter : {{ count }}</h1>
+        <button v-on:click="increment">Increment Button</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vuejs-dasar/src/counter.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+
+import Counter from "./components/Tugas1/Counter.vue";
+
+createApp(Counter).mount("#app");
